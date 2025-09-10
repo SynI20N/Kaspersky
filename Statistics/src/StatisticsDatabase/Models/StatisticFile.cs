71247b816960eab727918d6dc371459560d20e60
SynI20N <leonid.shcherbakov.01@mail.ru>
2025-09-10 10:06:05 +0300

    removed extra models

---
+++ Statistics/src/StatisticsDatabase/Models/StatisticFile.cs
@@ -0,0 +1,11 @@
+﻿
+namespace StatisticsDatabase.Models;
+
+public class StatisticFile
+{
+    public DateTime TimeStamp { get; internal set; }
+    public SeverityType Severity { get; internal set; }
+    public string? Product { get; internal set; }
+    public string? Version { get; internal set; }
+    public byte ErrorCode { get; internal set; }
+}
